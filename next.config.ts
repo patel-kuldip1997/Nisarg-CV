d93f4f85718db3c2fbb25d039bbc396e6e830c55
patelkuldip1997 <patelkuldip1997@gmail.com>
2026-08-20 13:39:25 +0530

    Portfolio

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.83.64.105', '10.83.65.121']
+  allowedDevOrigins: ['10.83.64.105', '10.83.65.121', '10.83.64.178']
 };
 
 export default nextConfig;
